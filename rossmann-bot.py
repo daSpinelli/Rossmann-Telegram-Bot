f8af44088458e5aa0bb338933c3c07830d10c645
daspinelli <denny.dealmeida@gmail.com>
2021-10-02 00:12:47 -0300

    new bot architecture to test new send_photo

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import json
 import requests
-from flask import Flask, request, Response
 import os
 import seaborn as sns
+import telegram
+import matplotlib.ticker as mticker
+
+from flask import Flask, request, Response
 
 # constants
 TOKEN = '1964746514:AAGmnUoclbp8R1NczhX38vt8_4Da10u4uW4'
@@ -20,56 +23,14 @@ TOKEN = '1964746514:AAGmnUoclbp8R1NczhX38vt8_4Da10u4uW4'
 # # send messages
 # https://api.telegram.org/bot1964746514:AAGmnUoclbp8R1NczhX38vt8_4Da10u4uW4/sendMessage?chat_id=1105720401&text=Hi!
 
-def send_message(chat_id, text):
-    parse = 'HTML'
-    url = 'https://api.telegram.org/bot{}/sendMessage'.format( TOKEN )
-    
-    message = {
-        'text': text,
-        'parse_mode': 'HTML',
-        'disable_web_page_preview': True,
-        'chat_id': chat_id
-    }
-    
-    header = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-    
-    print('Text: {}'.format(text))
-    r = requests.post(url, json=message, headers=header)
-    print('Status Code {}'.format(r.status_code))
-    print('Chat ID: {}'.format(chat_id))
+def send_msg(chat_id, text, _bot, parse='HTML'):
+    _bot.send_message(chat_id, text, parse_mode=parse)
 
     return None
 
-def send_photo(chat_id, photo_path, caption):
-    url = 'https://api.telegram.org/bot{}/sendPhoto'.format( TOKEN )
-    
-    chart_img = os.path.expanduser('~/{}'.format(photo_path))
-    
-    message = {
-        'photo': 'https://lh3.googleusercontent.com/t8jzr310rC5obgwRHRljXVYqpVt_LwPzoBoBknYxU9QH4sDPlItjYN_IGNbDIaK6d89pVC7XxGM87-QypJ9CmgHXSA=w640-h400-e365-rj-sc0x00ffffff',
-        'caption': caption,
-        'chat_id': chat_id
-    }
-    
-    header = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-    
-    f = os.path.isfile(chart_img)
-    if not f:
-        print('Não salvou!, {}'.format(chart_img))
-    else:
-        print('Salvou, {}'.format(chart_img))
-
-    
-    r = requests.post(url, json=message, headers=header)
-    print('Photo path: {}'.format(chart_img))
-    print('Status Code {}\nResponse {}\nText {}'.format(r.status_code, r.reason, r.text))
-    print('Chat ID: {}'.format(chat_id))
+def send_img(chat_id, img_path, caption):
+    img = open(img_path, 'rb')
+    _bot.send_photo(chat_id, img, caption=caption)
     
     return None
 
@@ -112,14 +73,24 @@ def predict(data):
 
     return d1
 
-def get_graph(predicted_data, x_axis, y_axis, title, x_label, y_label, img_name):
+def draw_chart(predicted_data, x_axis, y_axis, title, x_label, y_label, img_name):
+        tick_format = '{:,.0f}K'
+        
         fig = sns.barplot(x=x_axis, y=y_axis, data=predicted_data)
         fig.set_title(title)
         fig.set_xlabel(x_label)
         fig.set_ylabel(y_label)
-#         ylabels = ['{:,.0f}'.format(x) + 'K' for x in fig.get_yticks()/1000]
-#         fig.set_yticklabels(ylabels)
+        
+        fig.yaxis.set_major_locator(mticker.MultipleLocator(10000))
+        yticks = fig.get_ticks()
+        fig.yaxis.set_major_locator(mticker.FixedLocator(yticks))
+        
+        
+        ylabels = [tick_format.format(x) for x in yticks / 1000]
+        fig.set_yticklabels(ylabels)
+        
         fig.figure.savefig(img_name)
+        
         f = os.path.exists(img_name)
         if not f:
             print('Não salvou!')
@@ -169,6 +140,8 @@ Make good use of these data! With great powers comes great responsabilities!'
     
     return msg_help
 
+bot = telegram.Bot(token=TOKEN)
+
 # API initialize
 app = Flask(__name__)
 
@@ -195,10 +168,14 @@ def index():
         
         # filtered prediction
         if (type(command) == list) | (type(command) == int):
+            
             # reshape if there is only one store_id and convert list from string to int
             if type(command) == list:
+                
                 store_id = [int(x) for x in command]
+                
             else:
+                
                 store_id = [command,]
                 
             # loading data
@@ -213,34 +190,32 @@ def index():
                 d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
 
                 for i in range(len(d2)):
+                    
                 # send message
                     msg = 'Store Number {} will sell R${:,.2f} in the next 6 weeks'.format(
                         d2.loc[i, 'store'],
                         d2.loc[i, 'prediction']
                     )
-                    send_message( chat_id, msg )
-                    print('return message: {}'.format(msg))
-                    #return Response('Ok', status=200)
+                    send_msg(chat_id, msg, bot)
                 
             else:
-                send_message(chat_id, 'Store ID do not exist')
-                #return Response('Ok', status=200)
+                
+                send_msg(chat_id, 'Store ID do not exist', bot)
 
         # start
         elif (command == 'start'):
+            
             msg_help = get_help()
-            send_message(chat_id, msg_help)
-            #return Response('Ok', status=200)
+            send_message(chat_id, msg_help, bot)
             
         # help
         elif (command == 'help'):
+            
             msg_help = get_help(False)
-            send_message(chat_id, msg_help)
-            #return Response('Ok', status=200)
+            send_message(chat_id, msg_help, bot)
 
         # top prediction
-        elif command == 'toppredictions':
-            print('top predictions')            
+        elif command == 'toppredictions':     
             
             # loading data
             data = load_dataset(full=True)
@@ -252,43 +227,43 @@ def index():
             
             d3 = d2.nlargest(5, 'prediction')
             
-            # graph definitions
+            # chart definitions
             x_ax = 'store'
             y_ax = 'prediction'
             graph_title = 'Rossmann Sales Store Highest Predictions'
             x_lbl = 'Store ID'
             y_lbl = 'Predicion for next 6 weeks (Unit: K)'
-            image_path = 'top5_prediction.png'
+            image_path = './top5_prediction.png'
             
-            get_graph(
+            draw_chart(
                 d3,
                 x_axis=x_ax,
                 y_axis=y_ax,
-                title=graph_title,
+                title=chart_title,
                 x_label=x_lbl,
                 y_label=y_lbl,
                 img_name=image_path
             )
                         
-            send_photo(chat_id, image_path, graph_title)
-            #return Response('Ok', status=200)
+            send_img(chat_id, image_path, chart_title, bot)
 
         # top sales
         elif command == 'topsales':
+            
             print('top sales')
-            send_message(chat_id, 'top 5 sales')
-            #return Response('Ok', status=200)            
+            send_msg(chat_id, 'top 5 sales', bot)
             
         else:
+            
             msg_help = get_help(greeting = False)
-            send_message(chat_id, 'This is an invalid command!')
-            send_message(chat_id, msg_help)
-            #return Response('Ok', status=200)
+            send_message(chat_id, 'This is an invalid command!', bot)
+            send_message(chat_id, msg_help, bot)
         
-        send_message(chat_id, 'Done!')
+        send_message(chat_id, 'Done!', bot)
         return Response('Ok', status=200)
         
     else:
+        
         return '<h1> Rossmann Telegram BOT</h1>'
 
 if __name__ == '__main__':
